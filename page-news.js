100d0dab2dd6d086644f292cc4ec31f32cdc795e
alamedyang <alamedyang@gmail.com>
2017-06-16 20:40:05 -0600

    Actual content for "news" section

--- page-news.js
+++ page-news.js
@@ -5,8 +5,16 @@ let PageNews = Vue.component(
 	{
 		template: `
 			<section class="page-news">
-				<h1>News</h1>
-				<p>Peace customers, I'm OUT. Retiring. This is an archive/shrine/catalog that I'm keeping for memory's sake.</p>
+				<h1>News: Cosplay Scramble Ending in 2018</h1>
+				<p>Hello, everyone!</p>
+				<p>Cosplay Scramble has been a big part of my life for the last seven years, but my life has been moving in another direction lately so I’ve decided it’s time to move on.</p>
+				<p>I’m very grateful for all of you who love my magnets and who have supported me on Etsy and at conventions these many years. I hope my magnets continue to delight you on your fridges and lockers, whether you are “mixers” or “matchers!”</p>
+				<p>My Etsy shop will remain as it is now until I run out of magnets to sell, but I will be ramping down production in 2017. I am still hoping I can get to the point where commissions can reopen, probably in the summer or fall, but I have no news about that either way at this point.</p>
+ 				<p>Announcements about future convention attendance will be made here, though I can say that I’m shooting for Anime Banzai (SLC, UT) at least one last time.</p>
+ 				<p>Again, thank you for your support and for a wonderful seven years!</p>
+				<p> Cheers,<br>
+Mary Jenkins</p>
+				<p>P.S. Here’s Colonel Mustang in a miniskirt.</p>
 			</section>
 		`
 	}
