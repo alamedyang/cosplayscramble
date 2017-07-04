1883ff981676016ffbd28e01345070b63f1f8656
alamedyang <alamedyang@gmail.com>
2017-07-03 20:37:54 -0600

    Replaced placeholder content with text from the Google Doc

--- page-custom.js
+++ page-custom.js
@@ -5,8 +5,58 @@ let PageCustom = Vue.component(
 	{
 		template: `
 			<section class="page-custom">
-				<h1>Custom</h1>
-				<p>This page is going to grow up to be the most beautiful form even though it's an ugly duckling today.</p>
+				<h1>Custom orders</h1>
+				<p>I am currently accepting commissions and other custom orders! Commissions are expected to continue through October 2017 for fan art and mid-2018 for everything else.</p>
+
+				<p><strong>Current Commission Slots:</strong><br>
+1. <strong>rock</strong><br>
+2. <strong>tseng</strong><br>
+3. <em>empty!</em><br>
+4. <em>empty!</em><br>
+5. <em>empty!</em></p>
+
+				<h2>Types of Commissions</h2>
+				<p><strong>Fan Art:</strong> Any character from a commercial franchise. (See the <a href="#/catalog">General Catalog</a>.) Characters can be from anime, comics, games, television shows, movies, or anything else provided the parent company (and rights holder) is tolerant of fan art. This category is for characters that have a broad fanbase and that could be sold in the Artist Alley at anime conventions.</p>
+				<p><strong>OCs:</strong> Short for “original characters,” this category includes any original design of your own, including self-insert characters for commercial franchises. Common examples include fursonas, MLP ponysonas, and customized characters for popular anime. These are unique characters of your own design that would not be sold in the Artist Alley at anime conventions.</p>
+				<p><strong>People [head only]:</strong> This category is for drawings made from photos of you or anyone else you know in real life. For the time being, I will only draw heads from photos like this, meaning you will have to use bodies from other characters (to “cosplay”).</p>
+				<p><strong>Pets / Animals:</strong> I will draw your pet or a generic animal in a mini size.</p>
+				<p><strong>Color Edits:</strong> Any already-existing drawing can have its colors altered. (Lineart alterations count as a new commission in one of the above categories, though a reduced rate may be arranged depending on the scope of the alteration.)</p>
+				<p><strong>Other Special Orders:</strong> Other requests will be handled the same way as the next most-similar commission. E.g., a head-on-body non-decapitation order will be handled and charged like a POD magnet order.</p>
+
+				<h2>Prices</h2>
+				<p>{How do I table again?}</p>
+				<p>Remember that shipping is not included in the above chart, and will be at the regular rates. (See the <a href="#/order">Order</a> section for current rates.)</p>
+				<p><strong>Size:</strong> Cosplay Scramble magnets come in two sizes: large and small/mini. Generally humans and humanoids are the large size, while animals or small creatures, like Pokemon, are the small size.</p>
+				<p><strong>POD:</strong> This column indicates whether magnets are print-on-demand (as opposed to pre-made). Print-on-demand magnets are not made until ordered, and are hand cut unless ordered in bulk.</p>
+				<p><strong>Drawing:</strong> This is how much it costs to get the drawing made. The drawing cost also includes one copy of the finished drawing as a cut magnet.</p>
+				<p><strong>Cost:</strong> The first magnet is free, so this column is the cost of each magnet after the first if you want more than one. Special cases can be made for POD magnets that are ordered in bulk, which can be cost-effectively cut by machine. For the purposes of ordering in bulk, POD magnets that have the same cuttable outline count toward the total.</p>
+				<p><strong>Examples:</strong> Some examples of each category.</p>
+
+				<h2>Commission Procedure</h2>
+				<p><Each commissions slot allows someone to commission up to three new characters. Note that slots may be finished out of order depending on the drawings’ difficulty.</p>
+				<p>While I may decline or delay the commission due to subject matter or logistics issues, I will try to get as many done as I can this year. In advance I will say that I won’t draw anything controversial (i.e. lewd, politically charged, excessively violent).</p>
+				<p>There are limitations on poses and characters due to the format, so arms cannot be above the character’s head and thin pieces like tails or antenna will have to be thickened so the magnet won’t break off. In addition, Cosplay Scramble magnets cannot be made from characters without a workable jawline, such as characters whose entire body is their head (e.g. Jigglypuff) or bulky characters without anything resembling the anatomy of a neck (e.g. Totoro).</p>
+
+				<h4>Reserve a Slot</h4>
+				<p>To reserve a slot, <strong><a href="mailto:contact@cosplayscramble.com?Subject=Reserve%20a%20slot">email me</a> with the words “reserve a slot” in the subject line.</strong></p>
+				<p>I’ll also need to know who or what you’d like me to draw. If your commission is fan art, you’ll need to let me know if there’s a specific outfit you would prefer. If your commission is an original character, I’ll need head-to-toe reference. <strong>Attach or link any relevant images</strong> at this step. I might have questions about specific details, particularly for characters from franchises I’m not familiar with.</p>
+				<p>If you have any special requests, let me know at this point in case it will affect the final price. Special requests can include custom packaging (perhaps for a party), shipping to multiple addresses, non-decapitation, etc.</p>
+				<p>Once we’ve agreed on what I will be drawing, your slot is reserved! I will pick a codeword to represent your order in the Commissions Slot list above.</p>
+
+				<h4>Payment</h4>
+				<p>Once I’ve agreed to your commission, <strong>I will send a Paypal invoice to your email address.</strong> Paypal will let you pay in several ways, including with a credit card, so you won’t need a Paypal account to pay. The shipping rates are the same as for a general order — see the <a href="#/order">Order</a> section for current shipping prices.</p>
+				<p>(Magnets from the General Catalog can be ordered and shipped with your commission. Just let me know which sets you want, and I’ll include them on the invoice. You’ll receive all ordered magnets together.)</p>
+				<p>You may cancel your commission at any point, but the refund may be reduced depending on how far along I am. (E.g., if I haven’t started drawing yet you will get a full refund, but if I am done drawing but haven’t made the magnet yet you will only get the magnet manufacturing cost back.)</p>
+				<p>I do insist that the full commission cost is paid in advance. If payment takes too long, there’s a good change I will ask you to give up your slot (at least for the moment) so that others may have a turn.</p>
+
+				<h4>Production</h4>
+				<p><strong>Once payment clears, I’ll begin.</strong> Note: because some commissions will be more difficult than others, I may finish them out of order. I want them all to turn out well, and sometimes I need extra time to make sure I get them right.</p>
+				<p>When I’m done drawing, I’ll send you the finished image to see what you think. I can still make changes at this point, so it’s important to make sure I got everything about the character correct. (If I haven’t seen the anime the character is from, for example, I might miss an important detail.) Then, once we are both satisfied, <strong>I will print, magnet-ify, and cut the finished drawing(s). I’ll ship them as soon as they’re done, you’ll receive them in the mail!</strong></p>
+				<p>Fan art commissions will appear in the General Catalog at this point, and will also be available at any future conventions (while supplies last).</p>
+
+				<h2>Contact</h2>
+				<p>Questions? Send me an <a href="mailto:contact@cosplayscramble.com?Subject=A%20question">email me</a> or convo me on Etsy.</p>
+
 			</section>
 		`
 	}
--- page-order.js
+++ page-order.js
@@ -6,9 +6,67 @@ let PageOrder = Vue.component(
 		template: `
 			<section class="page-order">
 				<h1>Order</h1>
-				<p>Shout-out to all my homies buyin magnets!</p>
-				<p class="q">Q: Where did you find the beautiful background image of scrambled eggs you use as the background of your website?</p>
-				<p class="a">A: I was lucky enough to find it on <a href="https://www.flickr.com/photos/wackyland/8016622427/" target="_blank">Flickr by the artist "kyle tsui"</a>, generously provided under the Creative Commons 2.0 Attribution license. Thank you, Kyle!</p>
+				<p>To make an order, please <a href="mailto:contact@cosplayscramble.com?Subject=My%20Order">email me</a> with the product names you would like to buy and I will arrange a Paypal invoice — no Paypal account required. (Something more automated should be coming soon!) Anything listed in the Catalog or further down on this page is available to buy both online and in person at anime conventions. Let me know if you want priority shipping.</p>
+				<p>All prices are in US Dollars.</p>
+				<p>Customers not satisfied with the quality of their purchased magnets should contact me within one week of receiving them, along with a photo or scan of the unsatisfactory magnets. Such magnets may be eligible for free replacement. (Please save the packaging if you have a complaint, as the batch number can help me understand what problems you might be having.)</p>
+
+				<h2>Shipping</h2>
+				<p>Shipping rates: $3 to the US ($6.50 for priority), $9 to Canada, and $13 elsewhere. Priority to other countries is much more expensive and not much faster than first class mail, but can be arranged if required — please <a href="mailto:contact@cosplayscramble.com?Subject=Priority%20Shipping%20International">email me</a> for a price estimate.</p>
+				<p>Packages to the US usually arrive 2-4 days after shipping, and international orders usually arrive in 7-10 days, though packages can of course be randomly delayed, particularly when customs become involved. If prompt arrival is important, consider that priority mail has guarantees for delivery dates whereas first class mail does not.</p>
+				<p>Orders will be shipped within one or two days, but orders involving custom work will be shipped when the custom work is done.</p>
+				<p>I am not responsible for VAT or any other import fees, so please be aware of your country’s policies for international mail. (I am also unwilling to lie about the value of the package on custom forms.)</p>
+
+				<h2>Magnets</h2>
+				<p>Anything listed as in-stock in the <a href="#/catalog">General Catalog</a> is available to order online or at anime conventions where I have an Artist Alley table. (For a list of conventions I am attending, see the <a href="#/">Info</a> section.) Retired characters are not available for purchase except as a special order, which follow the procedure for POD magnets specified in the <a href="#/custom">Custom</a> section.</p>
+				<p>Substitutions are not possible for characters in a set. Nor are partial set sales possible, except in the case of already-orphaned magnets. (See below for information about orphans.)</p>
+
+				<h2>Grab Bags</h2>
+				<p>I have a variety of blind grab bags available online, as well as at conventions that allow them, as an affordable way to acquire characters in bulk. You will not be able to see what characters are included until you open the bag, but I sometimes limit the <em>category</em> of the bag to help narrow things down. (See the <a href="#/catalog">Catalog</a> to learn about what characters are available in each of the six color-coded categories.)</p>
+				<p>I make grab bags in batches, so while I don’t know exactly what each bag contains, I am theoretically able to prevent duplicates when grab bags are ordered together (provided they aren’t ordered in too-great of a quantity).</p>
+				<p><strong>Grab Bags</strong> are <strong>$5</strong>, and include <strong>at least 3 characters</strong> at a value of <strong>at least $10.</strong></p>
+				<p><strong>Super Grab Bags</strong> are <strong>$20</strong>, and include <strong>at least 13 characters</strong> at a value of <strong>at least $50.</strong></p>
+				<p><strong>Grab Bags Currently In Stock</strong><br>
+13x Grab Bags<br>
+6x Grab Bags (Anime)<br>
+4x Grab Bags (Video Game and Pokemon)<br>
+2x Grab Bags (Western Animation)</p>
+				<p><strong>Super Grab Bags Currently In Stock</strong><br>
+3x Super Grab Bag<br>
+1x Super Grab Bag (Anime)<br>
+1x Super Grab Bag (Video Game)</p>
+
+				<h2>Combos</h2>
+
+				<p>At conventions I offer discounts for certain combinations of sets. These are now also available online!</p>
+
+				<p><strong>All 8 Classic Doctors: $28</strong><br>
+First Doctor through Eighth Doctor. Save $4!</p>
+
+				<p><strong>All Doctor Who characters: $48</strong><br>
+First Doctor through Twelfth Doctor plus DW Companions Set 2. (DW Companions Set 1 is sold out and retired, unfortunately.) Save $12!</p>
+
+				<p><strong>All My Little Pony characters: $49</strong><br>
+Every My Little Pony character I have in stock. Save $23!</p>
+
+				<p><strong>All Pokémon characters: $85</strong><br>
+Every Pokémon character I have in stock. Currently includes 15 trainers and 37 Pokémon! Save $54!</p>
+
+				<p><strong>All Anime/Manga characters: $297</strong><br>
+All in-stock Anime/Manga characters (currently 103 total), 25% off! Save $99!</p>
+
+				<p><strong>All Video Game characters: $166.50</strong><br>
+All in-stock Video Game characters (currently 56 total), 25% off! Save $55.50!</p>
+
+				<p><strong>EVERY MAGNET I HAVE IN STOCK: $504</strong><br>
+WOW! All in-stock characters for $1.75 each! This is currently 288 characters! I’ll even throw in some extra, never-released characters! THIS IS MORE THAN 50% OFF! YOU’LL DROWN IN MAGNETS!</p>
+
+				<h2>Discounted Sets</h2>
+				<p>Previously convention-exclusive, discounted sets are now available online! Discounted sets are not flawed enough to be discarded but are flawed enough to warrant being sold at a reduced rate. Sometimes it’s as simple as a fleck of dust under the lamination layer that causes a bubble, or a misalignment of the cut edge by 1-1.5 mm.</p>
+				<p><strong>Coming Soon!</strong></p>
+
+				<h2>Orphans</h2>
+				<p>Sets with damaged pieces may leave orphaned characters/heads/bodies/accessories which are otherwise flawless. Such pieces were normally used at conventions are part of the “demo board” but are now available online! NOTE: Unlike magnets from the convention demo board, these orphans are in new condition unless mentioned otherwise.</p>
+				<p><strong>Coming Soon!</strong></p>
 			</section>
 		`
 	}
