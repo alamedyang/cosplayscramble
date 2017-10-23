7a5726092d077c9b0c69a2d09a704d98d85acadd
alamedyang <alamedyang@gmail.com>
2017-10-23 12:08:18 -0600

    made less wordy; added some combos; clarifications

--- page-order.js
+++ page-order.js
@@ -6,13 +6,16 @@ let PageOrder = Vue.component(
 		template: `
 			<section class="page-order">
 				<h1>Ordering</h1>
-				<p>To make an order, please <a href="mailto:cosplayscramble@gmail.com?Subject=My%20Order">email me</a> with the product names you would like to buy and I will arrange a Paypal invoice — no Paypal account required. (Something more automated should be coming soon!) Anything listed in the <router-link :to="\{ path: '/catalog'}">Catalog</router-link> or further down on this page is available to buy both online and in person at anime conventions. Let me know if you want priority shipping.</p>
+				<p><a href="mailto:cosplayscramble@gmail.com?Subject=My%20Order">Email me</a> with the product names you would like to buy and I will arrange a Paypal invoice. Everything in the <router-link :to="\{ path: '/catalog'}">Catalog</router-link> or further down on this page is available to buy both online and in person at anime conventions. Let me know if you want priority shipping.</p>
 				<p>All prices are in US Dollars.</p>
 				<p>Customers not satisfied with the quality of their purchased magnets should contact me within one week of receiving them, along with a photo or scan of the unsatisfactory magnets. Such magnets may be eligible for free replacement. (Please save the packaging if you have a complaint, as the batch number can help me understand what problems you might be having.)</p>
 
 				<h2>Shipping</h2>
-				<p>Shipping rates: $3 to the US ($6.50 for priority), $9 to Canada, and $13 elsewhere. Priority to other countries is much more expensive and not much faster than first class mail, but can be arranged if required — please <a href="mailto:cosplayscramble@gmail.com?Subject=Priority%20Shipping%20International">email me</a> for a price estimate.</p>
-				<p>Packages to the US usually arrive 2-4 days after shipping, and international orders usually arrive in 7-10 days, though packages can of course be randomly delayed, particularly when customs become involved. If prompt arrival is important, consider that priority mail has guarantees for delivery dates whereas first class mail does not.</p>
+				<p><strong>United States:</strong> $3 ($6.50 for priority)</br>
+<strong>Canada:</strong> $9</br>
+<strong>Worldwide:</strong> $13</p>
+				<p>Priority to other countries is much more expensive and not much faster than first class mail, but can be arranged if required — please <a href="mailto:cosplayscramble@gmail.com?Subject=Priority%20Shipping%20International">email me</a> for a price estimate.</p>
+				<p>Packages to the US usually arrive 2-4 days after shipping, and international orders usually arrive in 7-10 days, though packages can of course be randomly delayed. If prompt arrival is important, consider that priority mail has guarantees for delivery dates whereas first class mail does not.</p>
 				<p>Orders will be shipped within one or two days, but orders involving custom work will be shipped when the custom work is done.</p>
 				<p>I am not responsible for VAT or any other import fees, so please be aware of your country’s policies for international mail. (I am also unwilling to lie about the value of the package on custom forms.)</p>
 
@@ -21,7 +24,7 @@ let PageOrder = Vue.component(
 				<p>Substitutions are not possible for characters in a set. Nor are partial set sales possible, except in the case of already-orphaned magnets. (See below for information about orphans.)</p>
 
 				<h2>Grab Bags</h2>
-				<p>I have a variety of blind grab bags available online, as well as at conventions that allow them, as an affordable way to acquire characters in bulk. You will not be able to see what characters are included until you open the bag, but I sometimes limit the <em>category</em> of the bag to help narrow things down. (See the <router-link :to="\{ path: '/catalog'}">Catalog</router-link> to learn about what characters are available in each of the six color-coded categories.)</p>
+				<p>Blind grab bags are an affordable way to acquire characters in bulk. You will not be able to see what characters are included until you open the bag, but I sometimes limit the <em>category</em> of the bag to help narrow things down. (See the <router-link :to="\{ path: '/catalog'}">Catalog</router-link> to learn about what characters are available in each of the six color-coded categories.)</p>
 				<p><strong>Grab Bags</strong> are <strong>$5</strong>, and include <strong>at least 3 characters</strong> at a value of <strong>at least $10.</strong></p>
 				<p><strong>Super Grab Bags</strong> are <strong>$20</strong>, and include <strong>at least 13 characters</strong> at a value of <strong>at least $50.</strong></p>
 				<p>I make grab bags in batches known to contain no duplicate products. So while I don’t know exactly what each bag contains, I am theoretically able to prevent duplicates when grab bags are ordered together (provided they aren’t ordered in too-great of a quantity). If you want bags from a specific batch please let me know when you order.</p>
@@ -93,10 +96,25 @@ let PageOrder = Vue.component(
 				</div>
 				<h2>Combos</h2>
 
-				<p>At conventions I offer discounts for certain combinations of sets. These are now also available online!</p>
+				<p>Order together for a better deal!</p>
+
+				<p><strong>Ah! My Goddess Combo: $12</strong><br />
+Belldandy, Urd, and Skuld. Save $4!</p>
+
+				<p><strong>Evangelion Combo: $8</strong><br />
+Shinji, Rei, Asuka, Mari. Save $50%!</p>
+
+				<p><strong>Fruits Basket Combo: $12</strong><br />
+Tohru, Kyo (with cat) and Yuki (with mouse). Save $4!</p>
+
+				<p><strong>To Love Ru Combo: $8</strong><br />
+Lala Deviluke, Yuki Rito, Saki Tenjouin, and Yui Kotegawa. Save $50%!</p>
 
 				<p><strong>All 8 Classic Doctors: $28</strong><br />
 First Doctor through Eighth Doctor. Save $4!</p>
+
+				<p><strong>All Firefly Sets: $25</strong><br />
+All nine crew members. Save $11!</p>
 
 				<p><strong>All Remaining My Little Pony characters: $25</strong><br />
 Every My Little Pony character I have in stock. Save $17!</p>
@@ -134,7 +152,7 @@ Dot under lamination layer</p>
 				<p><strong>Firefly Set 2: $8</strong> (normally $12)<br />
 Dust under lamination layer; torn paper under lamination layer</p>
 				<p><strong>Deadpool: $2</strong> (normally $4)<br />
-Ripped foot</p>
+Ripped foot (repaired but with seam visible)</p>
 
 				<h2>Orphaned Singless</h2>
 				<p>Sets with damaged pieces may leave orphaned characters/heads/bodies/accessories which are otherwise flawless. Such pieces were normally used at conventions are part of the “demo board” but are now available online! NOTE: Unlike magnets from the convention demo board, these orphans are in new condition unless mentioned otherwise.</p>
